e92e2d45ecb698e2574ec452cd7353acffe287fc
Abdullah Alsabi <Abdullahb.alsabi@gmail.com>
2022-09-19 17:29:19 +0300

    Update variables_hw.py

--- variables_hw.py
+++ variables_hw.py
@@ -1,18 +1,18 @@
 # ---
 str = "Hello world"
 print(type(str))
-print(hex(id(str)))
+print(id(str))
 str +=  " from twuaiq!"
-print(hex(id(str)))
+print(id(str))
 
 print("observations: \n")
 
 # ---
 list = [1,2,3]
 print(type(list))
-print(hex(id(list)))
+print(id(list))
 list[0] = 5
-print(hex(id(list)))
+print(id(list))
 
 print("observations: \n")
 
@@ -20,20 +20,19 @@ print("observations: \n")
 # ---
 my_dict ={ "Ahmed": 43, "Mohammed": 23}
 print(type(my_dict))
-print(hex(id(my_dict)))
+print(id(my_dict))
 my_dict["Ali"] = "19"
-print(hex(id(my_dict)))
+print(id(my_dict))
 
 print("observations: \n")
 
 # --- 
 tuple = 1,2,3
 print(type(tuple))
-print(hex(id(tuple)))
+print(id(tuple))
 
 print("observations: \n")
 
 # # Uncomment the below code and run it, can u explain what happened?
 # tuple[0] = 2
 #print("observations: \n")
-
